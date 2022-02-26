bbb48a924aeed0c6ad2a639c36583a7052d937b2
Angyalne Zsuzsa <holczerzs@gmail.com>
2022-02-26 06:46:12 +0100

    ready

--- script.js
+++ script.js
@@ -23,30 +23,3 @@ function loadEvent() {
 
 
 window.addEventListener("load", loadEvent);
-
-// function init() {
-
-//     for (let i = 0; i < data.length; i++) {
-
-    
-//     let card;
-//     let title;
-//     let sub;
-//     let text;
-
-//     card = document.createElement("div")
-//     title = document.createElement("h2")
-//     sub = document.createElement("h3")
-//     text = document.createElement("h4")
-//     root = document.getElementById("root")
-
-//     card.setAttribute("id", "card" + i);
-//     card.setAttribute("data-index" + i);
-//     card.setAttribute("class", "data-card");
-//     root.appendChild(card);
-
-//     title.setAttribute("value", data[i].title);
-//     title.textContent = data[i].title;
-//     card.appendChild(title);
-// }
-// }
